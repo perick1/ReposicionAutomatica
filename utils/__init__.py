9ed739845460a65a91ee9a92645ae349ef70a448
perick1 <erickfeliperez@gmail.com>
2021-07-20 03:35:13 -0400

    initial update

---
+++ utils/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#from scripts._tools import *
+
+from utils._tablero import *#getBoard, plotBoard
+from utils._losetas import *#getPieces, plotMask
+from utils._caracteristicas import *
+from utils._match import *
+from utils._database import *
+from utils._results import *
+#from utils.chess import ChessBoard
