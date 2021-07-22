639dc66fa2f1fbe0b6c6602986f311dce36e67be
perick1 <erickfeliperez@gmail.com>
2021-07-22 03:25:59 -0400

    update

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,10 +3,4 @@
 
 #from scripts._tools import *
 
-from utils._tablero import *#getBoard, plotBoard
-from utils._losetas import *#getPieces, plotMask
-from utils._caracteristicas import *
-from utils._match import *
-from utils._database import *
-from utils._results import *
-#from utils.chess import ChessBoard
+from utils._objectivefx import *#getBoard, plotBoard
--- utils/_objectivefx.py
+++ utils/_objectivefx.py
@@ -1,7 +1,7 @@
 # Import modules
 import numpy as np
 
-def Beneficio(x ,A ,B ,S ,shape ,factor = 10**(10) ,penalty = 'constant'):
+def Beneficio1(x ,A ,B ,S ,shape ,factor = 10**(10) ,penalty = 'constant'):
     '''
     Entrega el beneficio de la empresa dada la venta de X productos con precios A
 
@@ -35,7 +35,6 @@ def Beneficio(x ,A ,B ,S ,shape ,factor = 10**(10) ,penalty = 'constant'):
         X = x[p,:].reshape(shape)
         f = np.sum(np.multiply(X,A))
         #restricciones
-        P = 10**(20)
         penalty = 0
         for t in range(T):
             Xt = X[:,:,t]
@@ -49,4 +48,49 @@ def Beneficio(x ,A ,B ,S ,shape ,factor = 10**(10) ,penalty = 'constant'):
                     penalty += P * factor
         score[p] = -f + penalty
 
+    return score
+
+def Beneficio2(x ,A ,B ,S ,shape ,factor = 10**(10) ,penalty = 'constant'):
+    '''
+    Entrega el beneficio de la empresa dada la venta de X productos con precios A
+
+    Parámetros:
+    ------------------
+    X       : numpy array de 3 indices
+            X[N,j,i*t] corresponde a la la partícula N que representa la reposición del SKU i en la tienda j en la semana k
+    A       : numpy array de 2 indices
+            A[j,i*t] corresponde al eneficio por venta del SKU i en la tienda j en la semana k
+    B       : numpy array de 2 indices
+            B[j,t] corresponde al stock máximo que puede albergar una tienda j en la semana t, contemplando todos los SKU
+    S       : numpy array de 1 indices
+            S[i*t] corresponde al máximo stock que se puede reponer en tiendas en la semana t del SKU i
+    shape   : tupla de tamaño 3 que da las dimensiones del problema.
+    factor  : float, factor por el cual se multiplica la penalizacion
+    penalty : str,  Tipo de penalizacion:
+              constant es un descuento constante por restriccion violada.
+              exponential es proporcional a la exponencial de la posicion de la particula por restriccion violada.
+    '''
+
+    #beneficio
+    X = x.reshape(shape)
+    revenue = np.sum(np.multiply(X,A))
+
+    #penalizacion por stock a repartir
+    stock_a_repartir = np.sum(X,axis=1)
+
+    #penalizacion por stock maximo en tienda
+    weeks = B.shape[1]
+    skus = int(shape[2]/weeks)
+    stock_por_tienda = np.zeros((shape[0],shape[1],weeks))
+    for i in range(weeks):
+        stock_por_tienda[:,:,i] = np.sum(X[:,:,i*skus:(i+1)*skus],axis=2)
+
+    if penalty == 'linear':
+        penalty1 = np.sum((stock_a_repartir > S) * (stock_a_repartir - S) * factor)
+        penalty2 = np.sum((stock_por_tienda > B) * (stock_por_tienda - B) * factor)
+    elif penalty == 'connstant':
+        penalty1 = np.sum((stock_a_repartir > S) * factor)
+        penalty2 = np.sum((stock_por_tienda > B) * factor)
+
+    score = penalty1 + penalty2 - revenue
     return score
